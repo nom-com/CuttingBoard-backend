0ded20ade568d999fb268da9416cdbd8d5751f50
= <jadenwilson717@gmail.com>
2020-12-18 12:52:03 -0700

    initial boilerplate code

---
+++ src/main/java/com/revature/utils/HibernateUtility.java
@@ -0,0 +1,21 @@
+package com.revature.utils;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtility {
+	
+	private static SessionFactory sessionFactory;
+	
+	public static Session getSession() {
+		if (sessionFactory == null) {
+			sessionFactory = new Configuration().setProperty("hibernate.connection.username", System.getenv("DB_USERNAME"))
+					.setProperty("hibernate.connection.password", System.getenv("DB_PASSWORD"))
+					.configure("hibernate.cfg.xml")
+					.buildSessionFactory();
+		}
+		
+		return sessionFactory.openSession();
+	}
+}
