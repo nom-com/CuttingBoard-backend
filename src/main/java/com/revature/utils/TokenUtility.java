c801b90cb3b5bb49ef0006923a709ad44d2b2d90
FrankIII <frj071196@gmail.com>
2020-12-21 20:09:36 -0600

    Added Java Web Token generator and decoder

---
+++ src/main/java/com/revature/utils/TokenUtility.java
@@ -0,0 +1,51 @@
+package com.revature.utils;
+
+import java.security.Signature;
+import java.util.Date;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import javax.xml.bind.DatatypeConverter;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+public class TokenUtility {
+	
+	private String SECRET_KEY = "secret";
+
+    public String createJWT(String id, String issuer, String subject, long ttlMillis) {
+
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        JwtBuilder builder = Jwts.builder().setId(id)
+                .setIssuedAt(now)
+                .setSubject(subject)
+                .setIssuer(issuer)
+                .signWith(signatureAlgorithm, signingKey);
+
+        if (ttlMillis >= 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        } 
+        
+        return builder.compact();
+        
+    }
+
+    public Claims decodeJWT(String jwt) {
+
+        Claims claims = Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .parseClaimsJws(jwt).getBody();
+        return claims;
+    }
+}
