8a204ffde85833ee0b9a2e61548c1d0cddfaaa5c
FrankIII <frj071196@gmail.com>
2020-12-24 13:42:07 -0600

    Changed LoginController and DTO

--- src/main/java/com/revature/cuttingboard/controller/LoginController.java
+++ src/main/java/com/revature/cuttingboard/controller/LoginController.java
@@ -1,29 +1,37 @@
 package com.revature.cuttingboard.controller;
 
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.revature.cuttingboard.dto.CredentialsDTO;
 import com.revature.cuttingboard.utils.TokenUtility;
 
-import io.jsonwebtoken.Claims;
-
 @RestController
-@RequestMapping("/login")
 public class LoginController {
 		
-		TokenUtility token = new TokenUtility();
+		@Autowired
+		TokenUtility token;
 		
-		@PostMapping(value="")
-		public String login(@RequestBody String username, String password ) {
-			if (username.equals("user") && password.equals("admin")) {
-				token.createJWT("1", "Nom.com", "testToken", 2000);
-				Claims userToken = token.decodeJWT("secret");
-				return username + password + userToken;
+		@Autowired
+		HttpServletResponse resp;
+		
+		@PostMapping(value="/login")
+		public CredentialsDTO login(@RequestBody CredentialsDTO user) {
+			if (user.getUsername().equals("user") && user.getPassword().equals("admin")) {
+				String userToken = token.createJWT("1", "Nom.com", user.getUsername(), 2000000);
+				resp.addHeader("Token", userToken);
+				return user;
 			} else {
 				return null;
 			}
 		}
+		
+		@PostMapping(value="/logout")
 }
