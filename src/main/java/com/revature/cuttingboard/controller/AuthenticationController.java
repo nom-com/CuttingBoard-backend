d8a66886a139bf660f2e656b39eff54a7caf6f9b
FrankIII <frj071196@gmail.com>
2020-12-24 23:09:57 -0600

    Added logout endpoint to end user session with token

--- src/main/java/com/revature/cuttingboard/controller/AuthenticationController.java
+++ src/main/java/com/revature/cuttingboard/controller/AuthenticationController.java
@@ -1,8 +1,11 @@
 package com.revature.cuttingboard.controller;
 
+import java.util.Date;
+
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -14,7 +17,7 @@ import com.revature.cuttingboard.dto.CredentialsDTO;
 import com.revature.cuttingboard.utils.TokenUtility;
 
 @RestController
-public class LoginController {
+public class AuthenticationController {
 		
 		@Autowired
 		TokenUtility token;
@@ -25,13 +28,26 @@ public class LoginController {
 		@PostMapping(value="/login")
 		public CredentialsDTO login(@RequestBody CredentialsDTO user) {
 			if (user.getUsername().equals("user") && user.getPassword().equals("admin")) {
-				String userToken = token.createJWT("1", "Nom.com", user.getUsername(), 2000000);
+				String userToken = token.createJWT("1", "Nom.com", user.getUsername(), 20000);
 				resp.addHeader("Token", userToken);
+				System.out.println(userToken);
 				return user;
 			} else {
 				return null;
 			}
 		}
 		
-		@PostMapping(value="/logout")
+		@GetMapping(value="/logout")
+		public String logout() {
+			if (token != null) {
+				long nowMillis = System.currentTimeMillis();
+				long expMillis = nowMillis * 0;
+				Date exp = new Date(expMillis);
+				token.setExpiration(exp);
+				System.out.println(token.getExpiration());
+				return "You logged out.";
+			} else {
+				return"";
+			}
+		}
 }
--- src/main/java/com/revature/cuttingboard/utils/TokenUtility.java
+++ src/main/java/com/revature/cuttingboard/utils/TokenUtility.java
@@ -1,6 +1,10 @@
 package com.revature.cuttingboard.utils;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.Map;
+import java.util.Set;
+
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 import javax.xml.bind.DatatypeConverter;
@@ -13,7 +17,7 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 
 @Component
-public class TokenUtility {
+public class TokenUtility implements Claims {
 	
 	private String SECRET_KEY = "secret";
 
@@ -50,4 +54,166 @@ public class TokenUtility {
                 .parseClaimsJws(jwt).getBody();
         return claims;
     }
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsKey(Object key) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean containsValue(Object value) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Object get(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object put(String key, Object value) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object remove(Object key) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void putAll(Map<? extends String, ? extends Object> m) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<String> keySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Collection<Object> values() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<Entry<String, Object>> entrySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getIssuer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Claims setIssuer(String iss) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getSubject() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Claims setSubject(String sub) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getAudience() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Claims setAudience(String aud) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Date getExpiration() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Claims setExpiration(Date exp) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Date getNotBefore() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Claims setNotBefore(Date nbf) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Date getIssuedAt() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Claims setIssuedAt(Date iat) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Claims setId(String jti) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public <T> T get(String claimName, Class<T> requiredType) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
