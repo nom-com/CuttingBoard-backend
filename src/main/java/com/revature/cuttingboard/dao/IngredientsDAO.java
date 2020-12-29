e88696d57df6dc977379008295b4d7a9ae2e24dd
= <jadenwilson717@gmail.com>
2020-12-28 19:52:16 -0700

    added ingredients endpoints, changed the expiration of the JWT so it is longer, fixed authAspect response status

---
+++ src/main/java/com/revature/cuttingboard/dao/IngredientsDAO.java
@@ -0,0 +1,104 @@
+package com.revature.cuttingboard.dao;
+
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.springframework.stereotype.Repository;
+
+import com.revature.cuttingboard.model.Ingredients;
+import com.revature.cuttingboard.utils.HibernateUtility;
+
+/**
+ * DAO class to handle database interactions pertaining to ingredients
+ * @author nom.com
+ * @since 1.0
+ *
+ */
+@Repository
+public class IngredientsDAO {
+
+	public List<Ingredients> getAllIngredients() throws Exception {
+		try (Session session = HibernateUtility.getSession()) {
+			Transaction tx = session.beginTransaction();
+			
+			CriteriaQuery query = session.getCriteriaBuilder().createQuery(Ingredients.class);
+			Root<Ingredients> root = query.from(Ingredients.class);
+			query.select(root);
+			
+			List<Ingredients> ingredients = session.createQuery(query).getResultList();
+			
+			tx.commit();
+			session.close();
+			return ingredients;
+		} catch (Exception e) {
+			throw new Exception("PSQL Error");
+		}
+	}
+	
+	public List<Ingredients> searchIngredient(String ingredient) throws Exception {
+		try (Session session = HibernateUtility.getSession()) {
+			Transaction tx = session.beginTransaction();
+			
+			List<Ingredients> ingredients = session.createQuery("FROM Ingredients i WHERE LOWER(i.ingredient) like LOWER(:ingredient)")
+					.setParameter("ingredient", "%" + ingredient + "%")
+					.list();
+			
+			tx.commit();
+			session.close();
+			return ingredients;
+		} catch (Exception e) {
+			throw new Exception("PSQL Error");
+		}
+	}
+	
+	public Ingredients insertIngredient(Ingredients ingredient) throws Exception {
+		try (Session session = HibernateUtility.getSession()) {
+			Transaction tx = session.beginTransaction();
+			
+			session.save(ingredient);
+			
+			tx.commit();
+			session.close();
+			return ingredient;
+		} catch (Exception e) {
+			throw new Exception("PSQL Error");
+		}
+	}
+	
+	public Ingredients updateIngredient(Ingredients ingredient) throws Exception {
+		try (Session session = HibernateUtility.getSession()) {
+			Transaction tx = session.beginTransaction();
+			
+			Ingredients dbIngredient = session.load(Ingredients.class, ingredient.getId());
+			dbIngredient.setIngredient(ingredient.getIngredient());
+			dbIngredient.setLastUpdatDate(ingredient.getLastUpdateDate());
+			dbIngredient.setLastUpdatedBy(ingredient.getLastUpdatedBy());
+			session.update(dbIngredient);
+			
+			tx.commit();
+			session.close();
+			return ingredient;
+		} catch (Exception e) {
+			throw new Exception("PSQL Error");
+		}
+	}
+	
+	public boolean deleteIngredient(int id) throws Exception {
+		try (Session session = HibernateUtility.getSession()) {
+			Transaction tx = session.beginTransaction();
+			
+			Ingredients ingredient = session.get(Ingredients.class, id);
+			session.delete(ingredient);
+			tx.commit();
+			session.close();
+			return true;
+		} catch (Exception e) {
+//			throw new Exception("PSQL Error");
+			throw new Exception(e.getMessage());
+		}
+	}
+}
---
+++ src/main/java/com/revature/cuttingboard/service/IngredientsService.java
@@ -0,0 +1,70 @@
+package com.revature.cuttingboard.service;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.cuttingboard.dao.IngredientsDAO;
+import com.revature.cuttingboard.dto.IngredientsDTO;
+import com.revature.cuttingboard.model.Ingredients;
+import com.revature.cuttingboard.model.SystemUser;
+
+/**
+ * Service class to handle business logic surrounding ingredients
+ * @author nom.com
+ * @since 1.0
+ *
+ */
+@Service
+public class IngredientsService {
+	
+	@Autowired
+	private IngredientsDAO ingredientsDao;
+	
+	public List<IngredientsDTO> getAllIngredients() throws Exception {
+		List<IngredientsDTO> ingredients = convertLists(ingredientsDao.getAllIngredients());
+		
+		return ingredients;
+	}
+	
+	public List<IngredientsDTO> searchIngredients(String ingredient) throws Exception {
+		List<IngredientsDTO> ingredients = convertLists(ingredientsDao.searchIngredient(ingredient));
+		
+		return ingredients;
+	}
+	
+	public IngredientsDTO insertIngredient(Ingredients ingredient, SystemUser user) throws Exception {
+		//set values not passed in by the user
+		Date today = new Date();
+		ingredient.setCreationDate(today);
+		ingredient.setLastUpdatDate(today);
+		ingredient.setCreatedBy(user);
+		ingredient.setLastUpdatedBy(user);
+		
+		return new IngredientsDTO(ingredientsDao.insertIngredient(ingredient));
+	}
+	
+	public IngredientsDTO updateIngredient(Ingredients ingredient, SystemUser user) throws Exception {
+		Date today = new Date();
+		ingredient.setLastUpdatDate(today);
+		ingredient.setLastUpdatedBy(user);
+		
+		return new IngredientsDTO(ingredientsDao.updateIngredient(ingredient));
+	}
+	
+	public boolean deleteIngredient(int id) throws Exception {
+		return ingredientsDao.deleteIngredient(id);
+	}
+	
+	private List<IngredientsDTO> convertLists(List<Ingredients> ingredientsList) {
+		List<IngredientsDTO> ingredients = new ArrayList<IngredientsDTO>();
+		for (Ingredients i : ingredientsList) {
+			ingredients.add(new IngredientsDTO(i));
+		}
+		
+		return ingredients;
+	}
+}
