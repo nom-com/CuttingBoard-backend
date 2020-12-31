7e3a7db4cd2aa9e9a57d26cfc86162e3d917696d
= <jadenwilson717@gmail.com>
2020-12-30 20:27:13 -0700

    removed appController contents

--- src/main/java/com/revature/cuttingboard/controller/AppController.java
+++ src/main/java/com/revature/cuttingboard/controller/AppController.java
@@ -13,34 +13,6 @@ import com.revature.cuttingboard.utils.HibernateUtility;
 
 public class AppController {
 	public static void main(String args[]) {
-		Session session = HibernateUtility.getSession();
-		Date today = new Date();
-		SystemUser user = new SystemUser();
-		user.setId(1);
-		RecipeAmount amount = new RecipeAmount();
-		Amount a = new  Amount();
-		a.setAmount("1 clove");
-		a.setCreatedBy(user);
-		a.setCreationDate(today);
-		a.setLastUpdatedBy(user);
-		a.setLastUpdateDate(today);
-		Ingredients i = new Ingredients();
-		i.setId(1);
-		a.setIngredient(i);
-		amount.setAmount(a);
-		amount.setCreatedBy(user);
-		amount.setCreationDate(today);
-		amount.setLastUpdatedBy(user);
-		amount.setLastUpdateDate(today);
-		Recipe r = new Recipe();
-		r.setId(1);
-		amount.setRecipe(r);
 		
-		session.save(amount);
-		
-		
-		System.out.println(amount);
-		
-		session.close();
 	}
 }
