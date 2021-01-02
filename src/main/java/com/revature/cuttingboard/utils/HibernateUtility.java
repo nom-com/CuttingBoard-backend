7e4bff63f47368e67f109c46293575e1c206126b
= <jadenwilson717@gmail.com>
2021-01-02 15:46:13 -0700

    debug lines for hibernate

--- src/main/java/com/revature/cuttingboard/utils/HibernateUtility.java
+++ src/main/java/com/revature/cuttingboard/utils/HibernateUtility.java
@@ -9,6 +9,7 @@ public class HibernateUtility {
 	private static SessionFactory sessionFactory;
 	
 	public static Session getSession() {
+		System.out.println(System.getenv("DB_PASSWORD"));
 		if (sessionFactory == null) {
 			sessionFactory = new Configuration().setProperty("hibernate.connection.url", System.getenv("DB_URL"))
 					.setProperty("hibernate.connection.username", System.getenv("DB_USERNAME"))
